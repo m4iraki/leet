455906bbb0705bc8067e9c2b95ba786d453ba8d8
Aleksandr Senchenko <awb.senchenko@gmail.com>
2026-04-29 20:53:28 +0300

    3. Longest Substring Without Repeating Characters

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,5 +2,5 @@ package io.github.m4iraki
 import solutions.*
 @main
 def main(): Unit = {
-  Solution.measure(AddTwoNumbers_2, runs = 5, warmup = 1)
+  Solution.measure(LongestSubstring_3, runs = 5, warmup = 1)
 }
---
+++ src/main/scala/solutions/LongestSubstring_3.scala
@@ -0,0 +1,40 @@
+package io.github.m4iraki
+package solutions
+
+object LongestSubstring_3 extends Solution[String, Int] {
+
+  import scala.annotation.tailrec
+
+  def lengthOfLongestSubstring(s: String): Int = {
+    val length = s.length
+    val lastSeen: Array[Int] =
+      Array.fill(Char.MaxValue - Char.MinValue)(-1)
+    @tailrec def inner(
+      pos: Int,
+      from: Int,
+      longest: Int,
+    ): Int =
+      if pos >= length then math.max(pos - from, longest)
+      else {
+        val char = s.charAt(pos)
+        val last = lastSeen(char)
+        lastSeen.update(char, pos)
+        if last >= from
+        then inner(pos + 1, last + 1, math.max(pos - from, longest))
+        else inner(pos + 1, from, longest)
+      }
+    inner(0, 0, 0)
+  }
+
+  def run: String => Int = lengthOfLongestSubstring
+
+  def samples: Seq[(String, Int)] = List(
+    "abcabcbb" -> 3,
+    "bbbbb" -> 1,
+    "pwwkew" -> 3,
+    "aab" -> 2,
+    "dvdf" -> 3,
+    "aabaab!bb" -> 3,
+  )
+
+}
