459d995711be55403e7c9def5072387fe893cb0b
Aleksandr Senchenko <awb.senchenko@gmail.com>
2026-05-11 00:04:04 +0300

    15. 3Sum single array.apply

--- src/main/scala/solutions/_1_to_1000/_1_to_100/_11_to_20/_15_ThreeSum.scala
+++ src/main/scala/solutions/_1_to_1000/_1_to_100/_11_to_20/_15_ThreeSum.scala
@@ -6,6 +6,7 @@ object _15_ThreeSum extends Solution[Array[Int], List[List[Int]]] {
   def threeSum(nums: Array[Int]): List[List[Int]] = {
     val sorted = nums.sorted
     val len = sorted.length
+
     @scala.annotation.tailrec
     def search(idx: Int, step: Int, limit: Int, prev: Int): Int =
       if idx == limit then limit
@@ -21,19 +22,25 @@ object _15_ThreeSum extends Solution[Array[Int], List[List[Int]]] {
       acc: List[List[Int]],
     ): List[List[Int]] =
       if idx >= len - 2 then acc
-      else if l >= r then {
-        val next = search(idx, 1, len - 2, sorted(idx))
-        loop(next, next + 1, len - 1, acc)
-      } else {
-        val sum = sorted(idx) + sorted(l) + sorted(r)
-        if sum == 0 then {
-          val nextL = search(l, 1, r, sorted(l))
-          val nextR = search(r, -1, l, sorted(r))
-          loop(idx, nextL, nextR, List(idx, l, r) :: acc)
-        } else if sum > 0 then loop(idx, l, search(r, -1, l, sorted(r)), acc)
-        else loop(idx, search(l, 1, r, sorted(l)), r, acc)
+      else {
+        val fst = sorted(idx)
+        if l >= r then {
+          val next = search(idx, 1, len - 2, fst)
+          loop(next, next + 1, len - 1, acc)
+        } else {
+          val snd = sorted(l)
+          val trd = sorted(r)
+          val sum = fst + snd + trd
+          if sum == 0 then {
+            val nextL = search(l, 1, r, snd)
+            val nextR = search(r, -1, l, trd)
+            loop(idx, nextL, nextR, List(fst, snd, trd) :: acc)
+          } else if sum > 0 then loop(idx, l, search(r, -1, l, trd), acc)
+          else loop(idx, search(l, 1, r, snd), r, acc)
+        }
       }
-    loop(0, 1, len - 1, Nil).map(_.map(sorted.apply)).distinct
+
+    loop(0, 1, len - 1, Nil)
   }
 
   import scala.collection.mutable.HashMap as MutableMap
