c121d5f035391937ea7c462945ce0bd2c28ef028
Aleksandr Senchenko <awb.senchenko@gmail.com>
2026-05-19 15:59:42 +0300

    28. Find the Index of the First Occurrence in a String naive and prettier implementation

--- src/main/scala/solutions/_1_to_1000/_1_to_100/_21_to_30/_28_IndexOfFirstOccurrence.scala
+++ src/main/scala/solutions/_1_to_1000/_1_to_100/_21_to_30/_28_IndexOfFirstOccurrence.scala
@@ -4,41 +4,28 @@ package solutions._1_to_1000._1_to_100._21_to_30
 object _28_IndexOfFirstOccurrence extends Solution[(String, String), Int] {
 
   def strStr(haystack: String, needle: String): Int = {
+
     val needleHead = needle.head
     val hlen = haystack.length
     val nlen = needle.length
+    val cutoff = hlen - nlen
+
     @scala.annotation.tailrec
-    def lookup(
-      hIdx: Int,
-      nIdx: Int,
-      fallbacks: List[Int],
+    def check(
+      hayIdx: Int,
+      ndlIdx: Int,
+    ): Boolean = (ndlIdx == nlen) ||
+      (haystack(hayIdx) == needle(ndlIdx) && check(hayIdx + 1, ndlIdx + 1))
+
+    @scala.annotation.tailrec
+    def loop(
+      idx: Int,
     ): Int =
-      if nIdx == nlen then hIdx - nIdx
-      else if nIdx == 0 && hIdx + nlen > hlen then
-        fallbacks match {
-          case head :: next =>
-            lookup(head + 1, 1, next)
-          case Nil => -1
-        }
-      else {
-        val hChar = haystack(hIdx)
-        val nChar = needle(nIdx)
-        val nFb =
-          if nIdx != 0 &&
-            hIdx + nlen <= hlen &&
-            hChar == needleHead &&
-            !fallbacks.contains(hIdx)
-          then hIdx :: fallbacks
-          else fallbacks
-        if hChar == nChar then lookup(hIdx + 1, nIdx + 1, nFb)
-        else
-          nFb match {
-            case head :: next =>
-              lookup(head + 1, 1, next)
-            case Nil => lookup(hIdx + 1, 0, Nil)
-          }
-      }
-    lookup(0, 0, Nil)
+      if idx > cutoff then -1
+      else if haystack(idx) == needleHead && check(idx + 1, 1) then idx
+      else loop(idx + 1)
+
+    loop(0)
   }
 
   def run: ((String, String)) => Int = strStr
@@ -49,6 +36,7 @@ object _28_IndexOfFirstOccurrence extends Solution[(String, String), Int] {
     ("mississippi", "issippi") -> 4,
     ("mississippi", "sippia") -> -1,
     ("leetcode", "leeto") -> -1,
+    ("h" * 1_000_000 + "e", "h" * 500_000 + "e") -> 500_000,
   )
 
 }
